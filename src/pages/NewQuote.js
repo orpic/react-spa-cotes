21ffe6ff82e3191792b5e310309e9c30b706d5f8
shobhit-u <shobhitumrao5@gmail.com>
2023-01-09 23:58:55 +0530

    http

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
 
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
+
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    sendRequest(quoteData);
+    // console.log(quoteData);
 
     ///navigate away prgrammatically
     /// useHistory
@@ -15,7 +27,10 @@ const NewQuote = () => {
   };
   return (
     <>
-      <QuoteForm onAddQuote={addQuoteHandler} />
+      <QuoteForm
+        isLoading={status === "pending"}
+        onAddQuote={addQuoteHandler}
+      />
     </>
   );
 };
