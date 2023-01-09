c68d1c0712836907adc9a7c2720e3f33c71ba939
shobhit-u <shobhitumrao5@gmail.com>
2023-01-09 22:32:41 +0530

    sorting qquery parameter added

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,9 +1,21 @@
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+//changes url or history
+import { useHistory } from "react-router-dom";
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const changeSortingHandler = () => {
+    // push for going back and replace for not going back
+    history.push("/quotes?sort=" + "ascending");
+  };
+
   return (
     <>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort Ascending</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
