8d2dee48e55cb19807232d486f258ae1bbb281cf
shobhit-u <shobhitumrao5@gmail.com>
2023-01-09 21:51:16 +0530

    dummy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Name 1",
+    text: "first text first text",
+  },
+  {
+    id: "q2",
+    author: "Name 2",
+    text: "second text second text",
+  },
+];
+
 const AllQuotes = () => {
   return <h1>AllQuotes</h1>;
 };
