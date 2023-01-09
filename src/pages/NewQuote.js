b24ca8ecaf895be0a4c4f131600d0d209991ce55
shobhit-u <shobhitumrao5@gmail.com>
2023-01-09 22:16:02 +0530

    programmatic nnavigation with useHostory and push

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,17 @@
 import React from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    ///navigate away prgrammatically
+    /// useHistory
+    //replace adds a redirect and cant go back
+    // push allows to go back
+    history.push("./quotes");
   };
   return (
     <>
