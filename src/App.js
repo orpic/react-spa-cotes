678ce119e1a4785ad9b22c41de6d862fc94b3c63
shobhit-u <shobhitumrao5@gmail.com>
2023-01-09 22:10:38 +0530

    404 Fallback NotFound.js complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
 import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -23,6 +24,10 @@ function App() {
 
           <Route path="/new-quote">
             <NewQuote />
+          </Route>
+          {/* //any url can match this, can only be added at last*/}
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </Layout>
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -3,14 +3,33 @@ import React from "react";
 import { Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Name 1",
+    text: "first text first text",
+  },
+  {
+    id: "q2",
+    author: "Name 2",
+    text: "second text second text",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
+
   return (
     <>
-      <h1>QuoteDetail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
